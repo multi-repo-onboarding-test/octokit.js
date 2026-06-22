ff76c83ed7a84579b1cabf97a52c2734f433129d
Aditya Murali <adityamurali@Adityas-MacBook-Pro.local>
2026-06-22 15:42:03 -0700

    test(e2e): add distinct app wiring change

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,9 @@ import { Octokit } from "./octokit.js";
 export const App = DefaultApp.defaults({ Octokit });
 export type App = InstanceType<typeof App>;
 
-export const OAuthApp = DefaultOAuthApp.defaults({ Octokit });
+export const OAuthApp = DefaultOAuthApp.defaults({
+  Octokit: App as unknown as typeof Octokit,
+});
 export type OAuthApp = InstanceType<typeof OAuthApp>;
 
 export { createNodeMiddleware } from "@octokit/app";
